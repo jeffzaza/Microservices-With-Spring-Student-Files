4c967d88540590bd175d487886c5f6b4b37fb8f2
tsangch <jeffzaza.jr@gmail.com>
2018-04-01 21:44:41 +0800

    [lab_20180401] Lab 1: Part 4 (Optional)- Create a Single Team endpoint

--- my-lab-1/src/main/java/com/example/demo/TeamController.java
+++ my-lab-1/src/main/java/com/example/demo/TeamController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.repo.Team;
@@ -19,5 +20,10 @@ public class TeamController {
 	@GetMapping("/teams")
 	public Iterable<Team> getTeams() {
 		return teamRepository.findAll();
+	}
+	
+	@GetMapping("/teams/{id}")
+	public Team getTeam(@PathVariable Long id) {
+		return teamRepository.findOne(id);		
 	}
 }
