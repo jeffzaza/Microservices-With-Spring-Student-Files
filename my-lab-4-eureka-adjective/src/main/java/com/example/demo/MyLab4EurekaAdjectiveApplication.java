8450c395444f73919b55843ce3508183825baf1f
tsangch <jeffzaza.jr@gmail.com>
2018-04-02 23:35:25 +0800

    [lab_20180401] Lab 4: Part 2, create clients, fix bugs

--- my-lab-4-eureka-adjective/src/main/java/com/example/demo/MyLab4EurekaAdjectiveApplication.java
+++ my-lab-4-eureka-adjective/src/main/java/com/example/demo/MyLab4EurekaAdjectiveApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class MyLab4EurekaSubjectApplication {
+public class MyLab4EurekaAdjectiveApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyLab4EurekaSubjectApplication.class, args);
+		SpringApplication.run(MyLab4EurekaAdjectiveApplication.class, args);
 	}
 }
--- my-lab-4-eureka-verb/src/test/java/com/example/demo/MyLab4EurekaVerbApplicationTests.java
+++ my-lab-4-eureka-verb/src/test/java/com/example/demo/MyLab4EurekaVerbApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MyLab4EurekaSubjectApplicationTests {
+public class MyLab4EurekaVerbApplicationTests {
 
 	@Test
 	public void contextLoads() {
