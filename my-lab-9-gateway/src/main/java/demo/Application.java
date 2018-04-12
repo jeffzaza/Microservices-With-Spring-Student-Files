412081e43e23dd2efbff9d2918d0fdb002d2bae6
tsangch <jeffzaza.jr@gmail.com>
2018-04-12 16:51:16 +0800

    [lab_20180401] Lab 9: Part 5 - Add ETag Support

--- my-lab-9-gateway/src/main/java/demo/Application.java
+++ my-lab-9-gateway/src/main/java/demo/Application.java
@@ -1,8 +1,12 @@
 package demo;
 
+import javax.servlet.Filter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -11,4 +15,9 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    
+    @Bean
+    public Filter shallowEtagHeaderFilter() {
+    	return new ShallowEtagHeaderFilter();
+    }  
 }
